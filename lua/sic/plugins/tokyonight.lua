092a5cc49724c502f84997fa9c70f4e8bc2b264f
saingchildren <80457007+saingchildren@users.noreply.github.com>
2024-07-22 07:10:14 +0800

    fix color

--- lua/sic/plugins/tokyonight.lua
+++ lua/sic/plugins/tokyonight.lua
@@ -6,12 +6,29 @@ return {
   config = function()
     local tokyonight = require("tokyonight")
     tokyonight.setup({
+      style = "storm",
       transparent = true,
       styles = {
         sidebars = "transparent",
         floats = "transparent",
-      }
+      },
+      on_highlights = function (hl, c)
+        local prompt = "#2d3149" 
+        hl.LineNr = {
+          fg = c.blue,
+          bg = "NONE"
+        }
+        hl.CursorLineNr = {
+          fg = c.yellow,
+          bg = "NONE",
+          bold = true,
+        }
+      end
     })
     tokyonight.load()
+    vim.cmd([[
+        hi NormalFloat guibg=#1e222a
+        hi FloatBorder guibg=#1e222a guifg=#3e4452
+      ]])
   end
 }
